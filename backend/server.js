b86ffdb031009faa5c3a9eeca4d9f8bb829bb061
subasthican <manoharansubasthican@gmail.com>
2026-04-02 10:36:18 +0530

    backend server.js and product routes fixed

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,20 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 import productRoutes from "./routes/productRoutes.js";
-const app = express();
 
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 mongoose
-    .connect("mongodb://127.0.0.1:27017/testdb")
-    .then(()=>console.log("Mongo Db connected"))
-    .catch(err=>console.log(err));
+  .connect("mongodb://127.0.0.1:27017/testdb")
+  .then(() => console.log("Mongo Db connected"))
+  .catch((err) => console.log(err));
 
+app.use("/api/products", productRoutes);
 
-
-app.listen(5001,()=>{
-    console.log("Server is running on pport 5001");
-})
+app.listen(5001, () => {
+  console.log("Server is running on port 5001");
+});
